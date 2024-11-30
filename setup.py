308f0f62c5ce494ba1fffc999f50ff3a5bfa40d9
Core World <159610152+coreworldyoutube@users.noreply.github.com>
2024-11-30 09:13:28 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='TSPY',  # ライブラリ名
+    version='0.1',      # バージョン
+    packages=find_packages(),
+    description='test',  # ライブラリの説明
+    author='Munetori',
+    author_like_foods='carryrice'
+    install_requires=[],  # 必要な依存関係があればここに書く
+)
