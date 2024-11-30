d385bdd0fe3de60c92526d5f6810c971132f4397
Core World <159610152+coreworldyoutube@users.noreply.github.com>
2024-11-30 10:28:55 +0900

    Update __init__.py

--- prog/__init__.py
+++ prog/__init__.py
@@ -1,10 +1,1 @@
-from google.colab import drive
-drive.mount('/content/drive')
-
-import sys
-sys.path.append('/content/drive/My Drive/your-repository-path/prog')
-
-from prog.itema import load_object_data, get_object_value
-
-# "gold" の値を取得
-print(get_object_value('gold'))  # 2
+from prog.itema import get_object_value, load_object_data
