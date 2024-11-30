cf4d15329c2f0007731ee2a93e343825dcd8c406
Core World <159610152+coreworldyoutube@users.noreply.github.com>
2024-11-30 09:54:41 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-# setup.py
 from setuptools import setup, find_packages
 
 setup(
     name='TerraSurvivalPy',  # ライブラリ名
-    version='0.1',      # バージョン
-    packages=find_packages(),
-    description='test',  # ライブラリの説明
-    author='Munetori'
-    install_requires=[],  # 必要な依存関係があればここに書く
+    version='0.1',           # バージョン
+    packages=find_packages(),  # パッケージを自動検出
+    description='test',      # ライブラリの説明
+    author='Munetori',       # 作者名
+    install_requires=[],     # 必要な依存関係があればここに書く
 )
+
