86977159c33927726b97bb1b93ff7958c0aee7bd
Core World <159610152+coreworldyoutube@users.noreply.github.com>
2024-11-30 10:22:20 +0900

    Update __init__.py

--- prog/__init__.py
+++ prog/__init__.py
@@ -1,1 +1,7 @@
+from google.colab import drive
+drive.mount('/content/drive')
+
+import sys
+sys.path.append('/content/drive/My Drive/your-repository-path/prog')
+
 from prog.itema import load_object_data, get_object_value
