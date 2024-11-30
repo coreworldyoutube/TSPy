f9a37b5b1040eb5f59da06f9faa3922791fa8961
Core World <159610152+coreworldyoutube@users.noreply.github.com>
2024-11-30 09:20:37 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='TSPY',  # ライブラリ名
+    name='Terra_Survival_PY',  # ライブラリ名
     version='0.1',      # バージョン
     packages=find_packages(),
     description='test',  # ライブラリの説明
