09e3a49c47cab77ae8e234c6c6ec72c07158585b
Core World <159610152+coreworldyoutube@users.noreply.github.com>
2024-12-03 09:23:38 +0900

    Update itema.py

--- prog/itema.py
+++ prog/itema.py
@@ -1,5 +1,3 @@
-# prog/itema.py
-
 import json
 import os
 
@@ -8,7 +6,8 @@ def load_object_data():
     JSONファイルからデータを読み込む関数。
     ファイルが存在しない場合はエラーメッセージを表示。
     """
-    json_path = os.path.join(os.path.dirname(__file__), 'json', 'object.json')  # JSONファイルのパス
+    # 現在のファイルのディレクトリパスを基に、'json/object.json'のパスを取得
+    json_path = os.path.join(os.path.dirname(__file__), 'json', 'object.json')
 
     # ファイルの存在確認と読み込み
     if not os.path.exists(json_path):
@@ -16,10 +15,3 @@ def load_object_data():
     
     with open(json_path, 'r') as file:
         return json.load(file)
-
-def get_object_value(key, data):
-    """
-    読み込んだデータから指定されたキーの値を取得する関数。
-    キーが存在しない場合は None を返す。
-    """
-    return data.get(key, None)  # キーが存在しない場合は None を返す
